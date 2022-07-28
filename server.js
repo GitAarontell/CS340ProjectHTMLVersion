a7a656d4d9d53ece1c4dd8c80204ed5f1e0ca69c
Your Name <adbertell2@gmail.com>
2022-07-27 17:49:31 -0700

    added server and connection files

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const app = express();
 const PORT = 65438;
 
 app.use(express.static('public'));
+let db = require('./connection.js');
+let connection = db.pool;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+app.get('/testing', (req, res) => {
+    
+    console.log(req.query)
+    connection.query('INSERT INTO tester(name) VALUES ("Bob");', (err, rows, fields) => {
+        if (err) {
+            console.log(err)
+        }
+    });
+    res.send('Hopefully it worked!');
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
